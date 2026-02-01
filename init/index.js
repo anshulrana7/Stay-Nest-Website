392339a94ebc39780bb76878bd0dfc4b7c24c095
anshulrana7 <ranaanshul821@gamil.com>
2026-02-01 06:15:36 +0530

    Fix mongodb dependency conflict

--- init/index.js
+++ init/index.js
@@ -1,79 +1,50 @@
-// // const mongoose = require("mongoose");
-// // const initData = require("./data.js");
-// // const Listing = require("../models/listing.js");
-// // const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-
-// // main().then(() =>{
-// //     console.log("Connected to db");
-// // }).catch((err)=>{
-// //     console.log(err);
-// // });
-
-// // async function main(params) { 
-// //     await mongoose.connect(MONGO_URL);
-// // }
-
-// // const initDB = async()=>{
-// //     await Listing.deleteMany({});
-// //     await Listing.insertMany(initData.data);
-// //     console.log("Data was initialized");
-// // }
-
-// // initDB();
-
-
 // const mongoose = require("mongoose");
 // const initData = require("./data.js");
 // const Listing = require("../models/listing.js");
-
 // const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
-// main()
-//   .then(() => {
-//     console.log("Connected to DB");
-//   })
-//   .catch((err) => {
+// main().then(() =>{
+//     console.log("Connected to db");
+// }).catch((err)=>{
 //     console.log(err);
-//   });
+// });
 
-// async function main() {
-//   await mongoose.connect(MONGO_URL); 
+// async function main(params) { 
+//     await mongoose.connect(MONGO_URL);
 // }
 
-// const initDB = async () => {
-//   await Listing.deleteMany({});  
-//   initData.data=initData.data.map((obj) =>({...obj, owner: "697b755942d8e883c322594a"}));
-//   await Listing.insertMany(initData.data);
-//   console.log("Data was initialized");
-// };
+// const initDB = async()=>{
+//     await Listing.deleteMany({});
+//     await Listing.insertMany(initData.data);
+//     console.log("Data was initialized");
+// }
 
 // initDB();
 
 
-require("dotenv").config({ path: "../.env" }); // 👈 FIX
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = process.env.ATLASDB_URL;
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
-  console.log("Connected to MongoDB Atlas");
+  await mongoose.connect(MONGO_URL); 
 }
 
 const initDB = async () => {
-  await Listing.deleteMany({});
-
-  initData.data = initData.data.map(obj => ({
-    ...obj,
-    owner: new mongoose.Types.ObjectId("697b755942d8e883c322594a")
-  }));
-
+  await Listing.deleteMany({});  
+  initData.data=initData.data.map((obj) =>({...obj, owner: "697b755942d8e883c322594a"}));
   await Listing.insertMany(initData.data);
   console.log("Data was initialized");
 };
 
-main()
-  .then(initDB)
-  .catch(err => console.log(err));
+initDB();
